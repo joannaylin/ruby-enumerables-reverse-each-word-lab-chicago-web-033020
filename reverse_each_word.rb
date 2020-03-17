2186725e9719d3569d88c5a4153ff99d6970b207
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 03:50:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,7 @@
-def reverse_each_word(sentence1)
-  array = sentence1.split
-  reversed_array = array.each do |word|
-    word.reverse!
-  end
-  reversed_array.join(" ")
-end
 
 def reverse_each_word(sentence1)
   array = sentence1.split.collect do |word|
     word.reverse!.join
   end
+  array
 end
