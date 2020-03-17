fc3fad061196a20504553f6be32eb212d934ef3f
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-17 03:17:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence1)
   new_array = []
 
   while i < array.length
-    reversed = array.each.reverse
+    reversed = array.reverse_each
     new_array << reversed
     i += 1
   end
